8d247339de473e794c87ce06a8f8f3c62f2870e2
andrelsmelo <andrelucassmelo@gmail.com>
2023-03-14 11:09:06 -0300

    att route names

--- api/src/routes.js
+++ api/src/routes.js
@@ -5,13 +5,10 @@ const clientController = require('./controllers/clientController');
 const registerController = require('./controllers/registerController');
 const categoryController = require('./controllers/categoryController');
 
-
-
 const userMiddleware = require('./middlewares/userMiddleware');
 const clientMiddleware = require('./middlewares/clientMiddleware');
 const registerMiddleware = require('./middlewares/registerMiddleware');
 const categoryMiddleware = require('./middlewares/categoryMiddleware');
-
 
 const router = express.Router();
 
@@ -20,25 +17,25 @@ router.get('/is-alive', (req,res) => {
 });
 
 
-router.get('/client', clientController.findAll);
+router.get('/clients', clientController.findAll);
 router.get('/client/:id', clientController.findOrFail);
 router.post('/client', clientMiddleware.validateBody, clientController.store);
 router.put('/client/:id', clientMiddleware.validateBody, clientController.update);
 router.delete('/client/:id', clientController.remove);
 
-router.get('/user', userController.findAll);
+router.get('/users', userController.findAll);
 router.get('/user/:id', userController.findOrFail);
 router.post('/user', userMiddleware.validateBody, userController.store);
 router.put('/user/:id', userMiddleware.validateBody, userController.update);
 router.delete('/user/:id', userController.remove);
 
-router.get('/category', categoryController.findAll);
+router.get('/categories', categoryController.findAll);
 router.get('/category/:id', categoryController.findOrFail);
 router.post('/category', categoryMiddleware.validateBody, categoryController.store);
 router.put('/category/:id', categoryMiddleware.validateBody, categoryController.update);
 router.delete('/category/:id', categoryController.remove)
 
-router.get('/register', registerController.findAll);
+router.get('/registers', registerController.findAll);
 router.get('/register/:id', registerController.findOrFail);
 router.post('/register', registerMiddleware.validateBody, registerController.store);
 router.put('/register/:id', registerMiddleware.validateBody, registerController.update);
