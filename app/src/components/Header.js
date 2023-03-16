54420718c1b8ab8140ba31428404fbfb9bc04ab3
Luca Kahn <lgamakahn@gmail.com>
2023-03-15 22:08:01 -0300

    add header and footer components

---
+++ app/src/components/Header.js
@@ -0,0 +1,26 @@
+
+import Button from "../components/Button";
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
+export default function Header() {
+
+    return(
+        <header>
+            <Link href="/"
+            style={{
+                textDecoration: 'none'
+            }}>
+                <div className={styles['header-title']}>
+                    <h1 className={'color-primary'}>FINANCE <span className='color-secondary'>MANAGEMENT</span></h1>
+                </div>
+            </Link>
+            <div className={styles['header-buttons']}>
+                <Button text="About Us" variant="primaryTransparent" />
+                <Link href="/login">
+                    <Button text="Login" variant="primaryOutlined" />
+                </Link>
+            </div>
+        </header>
+    )
+}
