2e6aa31290c2871dd59219f5b2bc1f0388b6b312
andrelsmelo <andrelucassmelo@gmail.com>
2023-03-20 19:55:32 -0300

    formating error messages

--- api/src/controllers/categoryController.js
+++ api/src/controllers/categoryController.js
@@ -4,10 +4,12 @@ const { saveLog } = require('../services/logService');
 const findAll = async (req, res) => {
     try {
         const categories = await categoryModel.findAll();
+
         saveLog('info', 'category/findAll', {}, 'Categories fetched successfully');
         return res.status(200).json(categories);
     } catch (error) {
         console.error(error);
+
         saveLog('error', 'category/findAll', {}, error.message);
         return res.status(500).json({ message: 'Error fetching Categories' });
     }
@@ -22,10 +24,12 @@ const findOrFail = async (req, res) => {
             saveLog('warn', 'category/findOrFail', req.params, 'Category not found');
             return res.status(404).json('Category not found');
         }
+
         saveLog('info', 'category/findOrFail', req.params, 'Category found successfully');
         return res.status(200).json(category);
     } catch (error) {
         console.error(error);
+
         saveLog('error', 'category/findOrFail', req.params, error.message);
         return res.status(500).json({ message: 'Error fetching category' });
     }
@@ -34,10 +38,12 @@ const findOrFail = async (req, res) => {
 const store = async (req, res) => {
     try {
         const createdCategory = await categoryModel.store(req.body);
+
         saveLog('info', 'category/store', req.body, 'Category created successfully');
         return res.status(201).json(createdCategory);
     } catch (error) {
         console.error(error);
+
         saveLog('error', 'category/store', req.body, error.message);
         return res.status(500).json({ message: 'Error creating category' });
     }
@@ -47,11 +53,14 @@ const update = async (req, res) => {
     try {
         const { id } = req.params;
         const updatedCategory = req.body;
+
         await categoryModel.update(id, updatedCategory);
+
         saveLog('info', 'category/update', req.params, 'Category updated successfully');
         return res.status(204).json();
     } catch (error) {
         console.error(error);
+
         saveLog('error', 'category/update', req.params, error.message);
         return res.status(500).json({ message: 'Error updating category', error });
     }
@@ -61,10 +70,12 @@ const remove = async (req, res) => {
     try {
         const { id } = req.params;
         await categoryModel.remove(id);
+
         saveLog('info', 'category/remove', req.params, 'Category removed successfully');
         return res.status(204).json();
     } catch (error) {
         console.error(error);
+
         saveLog('error', 'category/remove', req.params, error.message);
         return res.status(500).json({ message: 'Error deleting category', error});
     }
--- api/src/controllers/clientController.js
+++ api/src/controllers/clientController.js
@@ -4,10 +4,12 @@ const { saveLog } = require('../services/logService');
 const findAll = async (req, res) => {
   try {
     const clients = await clientModel.findAll();
+
     saveLog('info', 'client/findAll', {}, 'Clients fetched successfully');
     return res.status(200).json(clients);
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/findAll', {}, error.message);
     return res.status(500).json({ message: 'Error fetching clients', error });
   }
@@ -17,19 +19,17 @@ const findOrFail = async (req, res) => {
   try {
     const { id } = req.params;
     const client = await clientModel.findOrFail(id);
+    
     if (client.length === 0) {
       saveLog('warn', 'client/findOrFail', req.params, 'Client not found');
       return res.status(404).json('Client not found');
     }
-    saveLog(
-      'info',
-      'client/findOrFail',
-      req.params,
-      'Client found successfully'
-    );
+
+    saveLog('info', 'client/findOrFail', req.params, 'Client found successfully');
     return res.status(200).json(client);
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/findOrFail', req.params, error.message);
     return res.status(500).json({ message: 'Error fetching client', error });
   }
@@ -41,29 +41,25 @@ const findClientByUserid = async (req, res) => {
 
     const client = await clientModel.findClientByUserid(id);
 
-    saveLog(
-      'info',
-      'client/findClientByUserid',
-      req.params,
-      'Client found by user id successfully'
-    );
+    saveLog('info', 'client/findClientByUserid',req.params,'Client found by user id successfully');
     return res.status(200).json(client);
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/findClientByUserid', req.params, error.message);
-    return res
-      .status(500)
-      .json({ message: 'Error fetching client by user_id', error });
+    return res.status(500).json({ message: 'Error fetching client by user_id', error });
   }
 };
 
 const store = async (req, res) => {
   try {
     const createdClient = await clientModel.store(req.body);
+
     saveLog('info', 'client/store', req.body, 'Client created successfully');
     return res.status(201).json(createdClient);
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/store', req.body, error.message);
     return res.status(500).json({ message: 'Error creating client', error });
   }
@@ -80,6 +76,7 @@ const update = async (req, res) => {
     return res.status(204).json();
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/update', req.params, error.message);
     return res.status(500).json({ message: 'Error updating client', error });
   }
@@ -88,13 +85,13 @@ const update = async (req, res) => {
 const remove = async (req, res) => {
   try {
     const { id } = req.params;
-
     await clientModel.remove(id);
 
     saveLog('info', 'client/remove', req.params, 'Client removed successfully');
     return res.status(204).json();
   } catch (error) {
     console.error(error);
+
     saveLog('error', 'client/remove', req.params, error.message);
     return res.status(500).json({ message: 'Error removing client', error });
   }
