b1564774b74a884e14ba48a494295e5c71db6de5
andrelsmelo <andrelucassmelo@gmail.com>
2023-03-19 23:21:38 -0300

    add resume page

--- app/src/pages/resume.js
+++ app/src/pages/resume.js
@@ -1,7 +1,5 @@
-
 import Head from "next/head";
-import '../styles/Home.module.css';
-import Menu from "@/components/Menu";
+import styles from '../styles/Resume.module.css';
 
 export default function Resume() {
 
@@ -16,6 +14,42 @@ export default function Resume() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
+                <div className={styles["first-row"]}>
+                    <div className={styles["resume-container"]}>
+                        <div className={styles["resume-header"]}>
+                            Resume
+                        </div>
+                        <div className={styles["resume-history"]}>
+                            <ul>
+                                <li>AAA</li>
+                                <li>BBB</li>
+                                <li>CCC</li>
+                                <li>DDD</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles["register-container"]}>
+                        <button className={styles["new-register-btn"]}>
+                            Registrar
+                        </button>
+                        <div className={styles["resume-graphics"]}>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={styles["second-row"]}>
+                    <div className={styles["revenue-type-graphics"]}>
+                        <div className={styles["revenue-income"]}>
+
+                        </div>
+                        <div className={styles["revenue-outcome"]}>
+
+                        </div>
+                    </div>
+                    <div className={styles["balance"]}>
+
+                    </div>
+                </div>
             </main>
         </div>
     )
