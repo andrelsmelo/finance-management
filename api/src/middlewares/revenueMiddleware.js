a6c34c4b534a5e426dff8e587763d30ad2c79c56
andrelsmelo <andrelucassmelo@gmail.com>
2023-03-23 00:45:44 -0300

    add some front adjustments

--- api/src/middlewares/revenueMiddleware.js
+++ api/src/middlewares/revenueMiddleware.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const RevenueType = require('../enums/Revenue');
+const userModel = require('../models/user');
 
 const registerSchema = Joi.object({
   user_id: Joi.number().required(),
@@ -8,14 +9,21 @@ const registerSchema = Joi.object({
   category_id: Joi.number().required(),
 });
 
-const validateBody = (req, res, next) => {
+const validateBody = async (req, res, next) => {
   const { error } = registerSchema.validate(req.body);
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
 
-  const categoryId = req.body.category_id;
-  if (!Object.values(RevenueType).includes(categoryId)) {
+  const user_id = req.body.user_id;
+  const [user] = await userModel.findOrFail(user_id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const category_id = req.body.category_id;
+  if (!Object.values(RevenueType).includes(category_id)) {
     return res.status(400).json({ message: 'Invalid category_id' });
   }
 
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import KUTE from 'kute.js';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const tween = KUTE.fromTo(
+      '#blob1',
+      { path: '#blob1' },
+      { path: '#blob3' },
+      { repeat: 9999, duration: 3000, yoyo: true }
+    );
+
+    tween.start();
+  }, []);
   return (
     <div>
       <Head>
@@ -56,10 +68,52 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <div>
+          <svg
+            id='visual'
+            viewBox='0 0 900 600'
+            width='900'
+            height='600'
+            xmlns='http://www.w3.org/2000/svg'
+            xmlnsXlink='http://www.w3.org/1999/xlink'
+            version='1.1'
+          >
+            <g transform='translate(474.17176399009475 333.9872918157705)'>
+              <path
+                id='blob1'
+                d='M79.4 -154.1C105.1 -122.6 129.6 -105.8 144.5 -82.6C159.4 -59.3 164.7 -29.7 182.3 10.2C199.9 50 229.9 100 214.9 123.1C199.9 146.2 139.9 142.4 96.6 142.4C53.3 142.4 26.7 146.2 -3.1 151.5C-32.8 156.9 -65.7 163.7 -105.9 162C-146.2 160.2 -193.9 149.9 -225.8 121.4C-257.7 93 -273.9 46.5 -264.8 5.3C-255.7 -36 -221.4 -72 -185.5 -93.7C-149.7 -115.3 -112.4 -122.6 -81.2 -150.9C-50 -179.3 -25 -228.6 0.9 -230.2C26.8 -231.8 53.7 -185.6 79.4 -154.1'
+                fill='none'
+                className={styles['blob-line']}
+                strokeWidth='9'
+              ></path>
+            </g>
+{/*             <g
+              transform='translate(406.386090703667 328.9174203380071)'
+              style={{ visibility: 'hidden' }}
+            >
+              <path
+                id='blob2'
+                d='M98.1 -188.6C125.3 -154.3 144 -124.2 171.6 -93.4C199.2 -62.7 235.6 -31.3 245.3 5.6C254.9 42.5 237.9 85 212.1 118.8C186.2 152.5 151.6 177.6 114.8 190.5C78 203.4 39 204.2 1.1 202.3C-36.8 200.5 -73.7 195.9 -95.3 174.3C-117 152.6 -123.4 113.8 -132.5 81.9C-141.6 50 -153.3 25 -158.2 -2.8C-163.1 -30.7 -161.2 -61.3 -151.1 -91.5C-141.1 -121.7 -122.8 -151.3 -96.2 -186C-69.7 -220.7 -34.8 -260.3 0.3 -260.9C35.5 -261.5 71 -223 98.1 -188.6'
+                fill='none'
+                stroke='#BB004B'
+                strokeWidth='9'
+              ></path>
+            </g> */}
+            <g
+              transform='translate(472.03809690196823 292.45431208364545)'
+              style={{ visibility: 'hidden' }}
+            >
+              <path
+                id='blob3'
+                d='M101.7 -129C144.7 -108.8 201.2 -96.3 217.6 -66C234 -35.6 210.2 12.5 194.4 63.8C178.5 115.1 170.7 169.6 139.6 185.8C108.5 202.1 54.3 180 -0.2 180.3C-54.7 180.6 -109.3 203.1 -152.8 190.9C-196.2 178.7 -228.4 131.7 -248.6 78.6C-268.8 25.5 -277.1 -33.6 -249.9 -70.1C-222.7 -106.6 -160.2 -120.4 -112.6 -139.1C-65 -157.9 -32.5 -181.4 -1.6 -179.3C29.4 -177.1 58.8 -149.2 101.7 -129'
+                fill='none'
+                className={styles['blob-line']}
+                stroke-width='6'
+              ></path>
+            </g>
+          </svg>
+        </div>
       </main>
-      <div className={styles['bola']}>
-        <img src='/histerica.png' alt='histeric' />
-      </div>
     </div>
   );
 }
